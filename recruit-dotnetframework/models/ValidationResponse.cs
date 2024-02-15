d4056f0d310e266eac660081aaadbe0ccec73a55
MrGeet <aldarbatomunkuev@gmail.com>
2024-02-15 23:44:07 +1300

    Add validationResposne model

---
+++ recruit-dotnetframework/models/ValidationResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace recruit_dotnetframework.Models
+{
+    public class ValidationResponse
+    {
+        public bool IsValid { get; set; }
+        public string Message { get; set; }
+    }
+}
