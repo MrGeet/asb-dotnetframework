4836321d2ed61fbcad2ca06d6fec2f948bbda6d1
MrGeet <aldarbatomunkuev@gmail.com>
2024-02-15 23:49:03 +1300

    Update controller

--- recruit-dotnetframework/Controllers/CreditCardController.cs
+++ recruit-dotnetframework/Controllers/CreditCardController.cs
@@ -9,22 +9,31 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web.Http;
 
-
+using recruit_dotnetframework.ApiClients;
 
 namespace recruit_dotnetframework.Controllers
 {
     public class CreditCardController : ApiController
     {
-    
+        private readonly IExternalApiClient _externalApiClient;
 
-  
+        public CreditCardController(IExternalApiClient externalApiService)
+        {
+            _externalApiClient = externalApiService;
+        }
         [HttpPost]
         public async Task<IHttpActionResult> PostCardInformation(CreditCard creditCard)
         {
+            try
+            {
+                var result = await _externalApiClient.ValidateCreditCardInformation(creditCard);
 
-
-                return Ok();
-        
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
 
         }
     }
