35d76071e24802f6a7d3b49e40a0d67066d022e2
MrGeet <aldarbatomunkuev@gmail.com>
2024-02-15 23:41:56 +1300

    Create credit card controller

---
+++ recruit-dotnetframework/Controllers/CreditCardController.cs
@@ -0,0 +1,31 @@
+﻿using recruit_dotnetframework.Models;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+
+
+namespace recruit_dotnetframework.Controllers
+{
+    public class CreditCardController : ApiController
+    {
+    
+
+  
+        [HttpPost]
+        public async Task<IHttpActionResult> PostCardInformation(CreditCard creditCard)
+        {
+
+
+                return Ok();
+        
+
+        }
+    }
+}
