f94267f081f9a6e9a71e9160fdc2400c817a89d5
MrGeet <aldarbatomunkuev@gmail.com>
2024-02-15 23:42:23 +1300

    Create credit card model

---
+++ recruit-dotnetframework/models/CreditCard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace recruit_dotnetframework.Models
+{
+    public class CreditCard
+    {
+        [Required]
+        public string CardNumber { get; set; } 
+
+        [Required]
+        public string CVC { get; set; }
+
+        [Required]
+        public int ExpiryMonth { get; set; }
+
+        [Required]
+        public int ExpiryYear { get; set; }
+
+    }
+}
