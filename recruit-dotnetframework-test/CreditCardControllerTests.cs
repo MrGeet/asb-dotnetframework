2f2bb4cfe61b38f394302acc1c593ca8ca7890c0
MrGeet <aldarbatomunkuev@gmail.com>
2024-02-15 23:49:42 +1300

    Add unit tests

---
+++ recruit-dotnetframework-test/CreditCardControllerTests.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using recruit_dotnetframework.ApiClients;
+using recruit_dotnetframework.Controllers;
+using recruit_dotnetframework.Models;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Web.Services.Description;
+
+namespace recruit_dotnetframework_test
+{
+    [TestClass]
+    public class CreditCardControllerTests
+    {
+        private static CreditCardController CreateMockController(Mock<IExternalApiClient> mockApiClient)
+        {
+    
+
+            var config = new HttpConfiguration(); // Create a new HttpConfiguration
+            var request = new HttpRequestMessage();
+            var controller = new CreditCardController(mockApiClient.Object)
+            {
+                Configuration = config,
+                Request = request
+            };
+            return controller;
+        }
+        [TestMethod]
+        public async Task PostCardInformationTest_Success()
+        {
+            //arrange
+            var creditCard = new CreditCard
+            {
+                CardNumber = "123",
+                CVC = "123",
+                ExpiryMonth = 12,
+                ExpiryYear = 24,
+            };
+            var MockValidationResponse = new ValidationResponse { IsValid = true, Message = "Card validated successfully" };
+
+            var mockApiClient = new Mock<IExternalApiClient>();
+            mockApiClient.Setup(x => x.ValidateCreditCardInformation(It.IsAny<CreditCard>()))
+                .ReturnsAsync(MockValidationResponse);
+            var controller = CreateMockController(mockApiClient);
+            //act
+            var result = await controller.PostCardInformation(creditCard);
+            var response  = await result.ExecuteAsync(CancellationToken.None);
+            //assert
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+            Assert.IsTrue(response.TryGetContentValue(out ValidationResponse validationResponse));
+            Assert.AreEqual(validationResponse,MockValidationResponse);
+        }
+
+        [TestMethod]
+        public async Task PostCardInformationTest_Fail()
+        {
+            //arrange
+            var creditCard = new CreditCard
+            {
+                CardNumber = "123",
+                CVC = "123",
+                ExpiryMonth = 12,
+                ExpiryYear = 24,
+            };
+            var MockValidationResponse = new ValidationResponse { IsValid = true, Message = "Card validated successfully" };
+
+            var mockApiClient = new Mock<IExternalApiClient>();
+            mockApiClient.Setup(x => x.ValidateCreditCardInformation(It.IsAny<CreditCard>()))
+                .ThrowsAsync(new ArgumentException("Invalid Date"));
+            var controller = CreateMockController(mockApiClient);
+            //act
+            var result = await controller.PostCardInformation(creditCard);
+            var response = await result.ExecuteAsync(CancellationToken.None);
+            //assert
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.BadRequest);
+        }
+    }
+}
---
+++ recruit-dotnetframework-test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("recruit-dotnetframework-test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("recruit-dotnetframework-test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dd833c76-e10f-4bfa-b8b9-b32f58ee0b2c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
